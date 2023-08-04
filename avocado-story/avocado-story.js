df2606c2f167e57b544dd2f21f237c0550572eeb
Shanfan Huang <Shanfan@users.noreply.github.com>
2023-08-03 22:53:45 -0400

    Using seperate scrollTriggers for each scene seems to be overly entangling. Save a version here, and I'll branch off to try a single scrollTrigger to control the entire page.

--- avocado-story/avocado-story.js
+++ avocado-story/avocado-story.js
@@ -1,29 +1,127 @@
-let speed = 100;
+let masterTimeline = gsap.timeline();
 
-// Hide a bunch of things before the story starts
-gsap.set('#scene0 .content > p', {opacity: 0});
+const initialization = () => {
+  // Initialize a bunch of CSS properties to prepare for scoll magic!
+  gsap.set(".narration", {opacity: 0});
+
+  gsap.set(document.querySelectorAll(".scene"), {
+    height: '100vh',
+    backgroundColor: 'transparent',
+    display: 'block'
+  })
+
+  gsap.set('#s4 img', {
+    width: "auto",
+    position: "absolute",
+    left: "50%",
+    transform: "translateX(-50%",
+    opacity: 0
+  })
+
+  gsap.set('#s4 img:first-child', {opacity: 1})
+
+  gsap.set('#s5', {opacity: 0})
+}
+
+const scene0 = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#s0",
+      scrub: 1,
+      pin: ".content-wrapper",
+      pinSpacing: false,
+    }
+  });
+
+  tl.to(['#s0 .content > img', '#s0 .content > h1'], {y: -100, opacity: 0, stagger: 0.1})
+    .to('#s0 .content > p', { keyframes:[
+                                {y: -300, opacity: 1, duration: 0.5, ease: "power2.inOut"},
+                                {y: -500, opacity: 0, duration:0.1}]}, "<0.3")
+    .to('.content-wrapper', { backgroundColor: '#0a49b3', ease: "power1.inOut"}, "<")
+    .to('#cloud1', {y: "-30%", x: '-30%', opacity: 0.4, ease: "power1.inOut"}, "<")
+    .to('#cloud2', {y: "-40%", x: '30%', opacity: 0.3, ease: "power1.inOut"}, "<");
+
+  return tl;
+}
+
+const scene1 = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#s1',
+      scrub: 1,
+      start: 'top 70%',
+      end: 'bottom top',
+      markers: true
+    }
+  });
+
+  tl.to('.content-wrapper', { backgroundColor: '#000'}) // Think about it: should this tween go to the previous scene, and be a keyframe instead?
+    .from('#s1-lamp', {'--clip': "0%"}, "<")
+    .from('#s1-speech1', {opacity: 0, top: 20, ease: "power1.inOut"}, "<")
+    .from('#s1-speech2', {opacity: 0, top: 20, ease: "power1.inOut", delay: 0.1}, "<")
+    .to('#s1', {y: "-70%", ease: "none"}, "<")
+    .to('#s1-plant', {x: "-15%"}, "<")
+
+  return tl;
+};
+
+const scene2 = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#s2',
+      scrub: 1,
+    }
+  });
+
+  tl.to('.content-wrapper', {backgroundColor: "#b7e823"})
+    .to('#s2 img', {y: "300"}, ">")
+
+    return tl;
+}
+
+const scene3 = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#s3',
+      scrub: 1
+    }
+  });
+
+  tl.to('#s3', {y:"-500", duration: 0.1})
+    .to('#s3 img', {y: "500"},">")  
+    .to('.content-wrapper', {backgroundColor: "#eee1df"}, "<")
+    
+
+    return tl;
+}
+
+const scene4 = () => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#s4',
+      scrub: 1,
+    }
+  });
+
+  tl.to('.content-wrapper', {backgroundColor: "#eeeedf"})
+    .to('#s4 img', {opacity: "0.5", stagger: 0.2},"<")
+
+    return tl;
+}
 
 
-// Scene 0
-let scene0 = gsap.timeline();
-ScrollTrigger.create({
-  animation: scene0,
-  trigger: '.content-wrapper',
-  start: 'top top',
-  end: '15% 100%',
-  scrub: 1,
-  pin: '#scene0',
-  pinSpacing: false,
-});
 
-scene0.to('#scene0 .content > img', { y: -1 * speed, opacity: 0  }, 0);
-scene0.to('#scene0 .content > h1', { y: -2 * speed, opacity: 0  }, 0);
-scene0.to('#scene0 .content > p', { 
-    keyframes:[{y: -3 * speed, opacity: 1},{y: -3.5 * speed, opacity: 0}]  
-}, 0);
+const scene5 = document.querySelector('#s5');
+const scene6 = document.querySelector("#s6");
+const scene7 = document.querySelector("#s7");
 
-scene0.to('.content-wrapper', {
-    keyframes: [{backgroundColor: '#0a49b3'}, {backgroundColor: '#000'}]
-}, 0.3);
-scene0.to('#cloud1', {y: -0.5 * speed, x: '-30%', opacity: 0.4}, 0);
-scene0.to('#cloud2', {y: -1 * speed, x: '30%', opacity: 0.3}, 0);
+
+
+
+masterTimeline
+  .add(initialization())
+  .add(scene0())
+  .add(scene1())
+  .add(scene2())
+  .add(scene3())
+  .add(scene4());
